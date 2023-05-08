aee526f8a318020d0a6268fdb995e6ec1cc1c484
Sergio2 <sergio2@Sergios-MacBook-Pro.local>
2023-05-07 23:27:11 -0400

    add allow host

--- operation/operation/settings.py
+++ operation/operation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2ir_@_$gf54ao7*pxrp8!+1h@3^wfme1h5cqph082rxg3*0mr3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
